896efc99e59590f8aa9feec09ab676dc1304dd65
GalaArITT <oljagalavizar@ittepic.edu.mx>
2017-09-18 07:40:44 -0600

    Initial commit

---
+++ app/src/main/java/com/example/oliver/sqlite2_1/Registro.java
@@ -0,0 +1,41 @@
+package com.example.oliver.sqlite2_1;
+
+import android.database.Cursor;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class Registro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Instanciar clase DBAdapter
+        DBAdapter db = new DBAdapter(this);
+        //---Añadir datos
+        db.open();
+        //long id = db.insertContact("Naty", "naty@ejemplo.com");
+        //id = db.insertContact("Oscar Lopez", "olopez@mail.com");
+        db.close();
+        db.open();
+        // Explorar el cursos
+        Cursor c = db.getAllContacts();
+        if (c.moveToFirst()) {
+            do {
+                DisplayContact(c);
+            } while (c.moveToNext());
+        }
+        db.close();
+    }
+    // Desplegar el contenido vía Toast
+    public void DisplayContact(Cursor c) {
+        Toast.makeText(this,
+                "id: " + c.getString(0) + "\n" +
+                        "Name: " + c.getString(1) + "\n" +
+                        "Email: " + c.getString(2),
+                Toast.LENGTH_LONG).show();
+    }
+
+}
